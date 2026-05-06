17614015e910e68bf25ee40ab13bb499947d3308
LiLQD <luuquangdung1511@gmail.com>
2026-05-06 20:27:35 +0700

    refactor(ui): remove static html skeleton before DOM redering rewrite

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import "./styles.css"
+import { homeLoad } from "./home.js";
+import { menuLoad } from "./menu.js";
+import { contactLoad } from "./contact.js";
+export const content = document.querySelector("#content");
+
+
 console.log("hi");
+
